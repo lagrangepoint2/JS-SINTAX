de0020141cc79a19bb455ad5fdda16f70b122a8b
Martin Perez <martinperezsh@gmail.com>
2017-10-10 18:51:38 -0600

    Updated course page and quiz page for first course

--- public/assets/js/quiz.js
+++ public/assets/js/quiz.js
@@ -3,31 +3,31 @@ $(document).ready(function(){
 });
 
 var questions = [
-	{
-		question: "What would you use to wrap around in array?",
-		answers: ['( )', '[ ]', '" "', '{ }'],
-		correctAnswer: '[ ]'
-	}, 
-	{
-		question: 'What type of scope is it when a variable is not in a function?',
-		answers: ['Local Scope', 'No Scope', 'Global Scope', 'Function Scope'],
-		correctAnswer: 'Global Scope'
-	},
-	{
-		question: 'What do you use console.log(); for?',
-		answers: ['Leaving notes for your buddies', 'Debugging and testing', 'Leave comments on your code', 'Having an alert pop out in your website'],
-		correctAnswer: 'Debugging and testing'
-	},
-	{
-		question: 'If you want the value of the number five, how would you write it out?',
-		answers: ['5', '"five"', 'five', '"5"'],
-		correctAnswer: '5' 
-	},
-	{
-		question: 'What do you start a line with to leave a comment in your code?',
-		answers: ['<--', '/*', '""', '//'],
-		correctAnswer: '//'
-	}
+  {
+    question: "Which of the following would you use to wrap around an array?",
+    answers: ['( )', '[ ]', '" "', '{ }'],
+    correctAnswer: '[ ]'
+  }, 
+  {
+    question: 'What type of scope is it when a variable is not in a function?',
+    answers: ['Local Scope', 'No Scope', 'Global Scope', 'Function Scope'],
+    correctAnswer: 'Global Scope'
+  },
+  {
+    question: 'What do you use console.log(); for?',
+    answers: ['Leaving notes for your buddies', 'Debugging and testing', 'Leave comments on your code', 'Having an alert pop out in your website'],
+    correctAnswer: 'Debugging and testing'
+  },
+  {
+    question: 'If you want the value of the number five, how would you write it out?',
+    answers: ['5', '"five"', 'five', '"5"'],
+    correctAnswer: '5' 
+  },
+  {
+    question: 'What do you start a line with to leave a comment in your code?',
+    answers: ['<--', '/*', '""', '//'],
+    correctAnswer: '//'
+  }
 ];
 
 var quiz = {
@@ -38,6 +38,7 @@ var quiz = {
     $('#quizBtn').on('click', function(){
       event.preventDefault();
       quiz.done();
+    $('#quizBtn').hide();
     });
     quiz.counter--;
     $('#counter').html(quiz.counter);
@@ -49,45 +50,43 @@ var quiz = {
 //timer decrease time by 1 second
     timer = setInterval(quiz.countdown, 1000);
 // Put timer in html
-    $('#wrapper').prepend('<h2> Time Remaining: <span id="counter">30</span> Seconds </h2>')
+    $('#wrapper').prepend('<h2> Time Remaining: <span id="counter"></span> Seconds </h2>')
 // Put the questions and answers in the html
-    for (var i =0; i < questions.length; i++){
-      $('#wrapper').append('<h2>' + questions[i].question + '</h2>')
+    for (var i =0; i < questions.length; i++){ 
       for (var j = 0; j < questions[i].answers.length; j++){
-        $('#wrapper').append('<input type="radio" name="question-' + i + '" value="' + questions[i].answers[j] + '">' + questions[i].answers[j])
       }
     }
   },
   done: function(){
-    $.each($('input[name="question-0"]:checked'), function(){
+    $.each($('input[id="q1-2"]:checked'), function(){
       if($(this).val() === questions[0].correctAnswer){
         quiz.correct++;
-      } else {
+      } else if($(this).val() != questions[0].correctAnswer) {
         quiz.sins++;
       }
     });
-    $.each($('input[name="question-1"]:checked'), function(){
+    $.each($('input[id="q2-3"]:checked'), function(){
      if($(this).val() === questions[1].correctAnswer){
        quiz.correct++;
      } else {
        quiz.sins++;
      }
    });
-   $.each($('input[name="question-2"]:checked'), function(){
+   $.each($('input[id="q3-2"]:checked'), function(){
      if($(this).val() === questions[2].correctAnswer){
        quiz.correct++;
      } else {
        quiz.sins++;
      }
    });
-   $.each($('input[name="question-3"]:checked'), function(){
+   $.each($('input[id="q4-1"]:checked'), function(){
       if($(this).val() === questions[3].correctAnswer){
         quiz.correct++;
       } else {
         quiz.sins++;
       }
    });
-   $.each($('input[name="question-4"]:checked'), function(){
+   $.each($('input[id="q5-4"]:checked'), function(){
       if($(this).val() === questions[4].correctAnswer){
         quiz.correct++;
       } else {
@@ -102,8 +101,7 @@ var quiz = {
     $('#wrapper h2').remove();
     $('#wrapper').html('<h2>Quiz Completed!</h2>');
     $('#wrapper').append('<h3>Correct Answers: ' + this.correct + '</h3>');
-    $('#wrapper').append('<h3>Sins: ' + this.sins + '</h3>');
-    $('#wrapper').append('<h3>Unanswered: ' +(questions.length - (this.sins + this.correct)) + '</h3>');
+    $('#wrapper').append('<h3>Sins: ' +(questions.length - (this.correct)) + '</h3>');
   }
-//  closes game function 
+
 };
